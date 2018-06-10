a9acef59aff857254b8ada924a9855c429fdd594
sean-blattenberger <sean.blattenberger@gmail.com>
2018-06-09 21:56:53 -0600

    Added data

--- instructors.js
+++ instructors.js
@@ -1,74 +1,77 @@
-module.exports = [
-  {
-    ID: 1,
-    "Full Name": "Kyle Coberly",
-    Title: "Faculty Director",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 2,
-    "Full Name": "Danny Fritz",
-    Title: "Lead Instructor",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 3,
-    "Full Name": "CJ Reynolds",
-    Title: "Lead Instructor",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 4,
-    "Full Name": "Brooks Patton",
-    Title: "Lead Instructor",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 5,
-    "Full Name": "Roberto Ortega",
-    Title: "Lead Instructor",
-    "Number of Dogs": 1
-  },
-  {
-    ID: 6,
-    "Full Name": "Chad Drummond",
-    Title: "Instructor",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 7,
-    "Full Name": "Kim Schlesinger",
-    Title: "Instructor",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 8,
-    "Full Name": "Peter Ostiguy",
-    Title: "Associate Instructor",
-    "Number of Dogs": 1
-  },
-  {
-    ID: 9,
-    "Full Name": "Cass Torske",
-    Title: "Resident",
-    "Number of Dogs": 1
-  },
-  {
-    ID: 10,
-    "Full Name": "Matt Winzer",
-    Title: "Resident",
-    "Number of Dogs": 2
-  },
-  {
-    ID: 11,
-    "Full Name": "Aaron Goodman",
-    Title: "Resident",
-    "Number of Dogs": 0
-  },
-  {
-    ID: 12,
-    "Full Name": "Michelle Bergquist",
-    Title: "Resident",
-    "Number of Dogs": 1
-  }
-];
+module.exports = {
+  data:
+    [
+      {
+        id: 1,
+        "fullName": "Kyle Coberly",
+        "title": "Faculty Director",
+        "numberOfDogs": 0
+      },
+      {
+        id: 2,
+        "fullName": "Danny Fritz",
+        "title": "Lead Instructor",
+        "numberOfDogs": 0
+      },
+      {
+        id: 3,
+        "fullName": "CJ Reynolds",
+        "title": "Lead Instructor",
+        "numberOfDogs": 0
+      },
+      {
+        id: 4,
+        "fullName": "Brooks Patton",
+        "title": "Lead Instructor",
+        "numberOfDogs": 0
+      },
+      {
+        id: 5,
+        "fullName": "Roberto Ortega",
+        "title": "Lead Instructor",
+        "numberOfDogs": 1
+      },
+      {
+        id: 6,
+        "fullName": "Chad Drummond",
+        "title": "Instructor",
+        "numberOfDogs": 0
+      },
+      {
+        id: 7,
+        "fullName": "Kim Schlesinger",
+        "title": "Instructor",
+        "numberOfDogs": 0
+      },
+      {
+        id: 8,
+        "fullName": "Peter Ostiguy",
+        "title": "Associate Instructor",
+        "numberOfDogs": 1
+      },
+      {
+        id: 9,
+        "fullName": "Cass Torske",
+        "title": "Resident",
+        "numberOfDogs": 1
+      },
+      {
+        id: 10,
+        "fullName": "Matt Winzer",
+        "title": "Resident",
+        "numberOfDogs": 2
+      },
+      {
+        id: 11,
+        "fullName": "Aaron Goodman",
+        "title": "Resident",
+        "numberOfDogs": 0
+      },
+      {
+        id: 12,
+        "fullName": "Michelle Bergquist",
+        "title": "Resident",
+        "numberOfDogs": 1
+      }
+    ]
+}
